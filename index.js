f1561970869386a766b4c6365c6b18da82fd010e
Wilbur125 <matt.s.wilbur@gmail.com>
2020-12-12 03:58:22 -0800

    Created index js file, installed inquirer and mysql, and created the connection for mysql database

---
+++ index.js
@@ -0,0 +1,84 @@
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "password",
+  database: "employees"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  runSearch();
+});
+
+function runSearch() {
+  inquirer
+    .prompt({
+      name: "action",
+      type: "rawlist",
+      message: "What would you like to do?",
+      choices: [
+        "Add departments?",
+        "Add roles?",
+        "Add employees?",
+        "View departments?",
+        "View roles?",
+        "View employees?",
+        "Update employee roles?"
+      ]
+    })
+    .then(function(answer) {
+      switch (answer.action) {
+      case "Add departments?":
+        addDepts();
+        break;
+
+      case "Add roles?":
+        addRoles();
+        break;
+
+      case "Add employees?":
+        addEmployees();
+        break;
+
+      case "View departments?":
+        viewDepts();
+        break;
+
+      case "View roles?":
+        viewRoles();
+        break;
+
+        case "View employees?":
+        viewEmployees();
+        break;
+
+        case "Update employee roles?":
+        updateRoles();
+        break;
+      }
+    });
+}
+
+function addDepts() {
+  inquirer
+    .prompt({
+      name: "department",
+      type: "input",
+      message: "What department would you like to add?"
+    })
+    .then(function(answer) {
+      var query = "INSERT INTO departments SET ?";
+      connection.query(query, { 
+          department_name: answer.department 
+        }, 
+        function(err, res) {
+            if (err) throw err;
+            console.log("The department was added successfully!");
+        runSearch();
+      });
+    });
+}
